b85203ca62d6e65a4a59537f066ee5495b9b2840
Reinaldo Ignatius Wijaya <reinaldo@agate.id>
2018-02-06 22:30:33 +0700

    Added Chapter Select, Title Screen, and Chapter 1

---
+++ Assets/Scripts/chapterSelectController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VR;
+
+public class chapterSelectController : MonoBehaviour {
+
+	private const string VR_DEVICE_NAME = "cardboard";
+	private const string CHAPTER_SELECT_SCENE = "Chapter Select";
+	private const string TITTLE_SCREEN_SCENE = "Title Screen";
+	private const string CHAPTER_1_SCENE = "Circus";
+
+	private IEnumerator runInVRMode (string scene) {
+		VRSettings.LoadDeviceByName (VR_DEVICE_NAME);
+		yield return null;
+		VRSettings.enabled = true;
+		yield return new WaitForSeconds(0.1f);
+		SceneManager.LoadScene (scene, LoadSceneMode.Single);
+	}
+
+	private void goToScene (string scene) {
+		if (SceneManager.GetActiveScene ().name == CHAPTER_SELECT_SCENE) {
+			StartCoroutine (runInVRMode (scene));
+		} else {
+			SceneManager.LoadScene (scene, LoadSceneMode.Single);
+		}
+	}
+
+	public void goToTitleScreen () {
+		goToScene (TITTLE_SCREEN_SCENE);
+	}
+
+	public void goToChapter1 () {
+		goToScene (CHAPTER_1_SCENE);
+	}
+
+}
