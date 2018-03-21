fdb950c5b12ced96aacdf979ec190705ed06ff9e
Reinaldo Ignatius Wijaya <rei_iw@yahoo.com>
2018-03-21 23:11:40 +0700

    Fixed pointer arrow

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -14,39 +14,56 @@ public class Pointer : MonoBehaviour {
 		}
 	}
 		
+	public const int NULL = -1;
+
 	[SerializeField]
 	private float MAGNITUDE_THRESHOLD = 0.75f;
 	[SerializeField]
 	private Transform cameraTransform;
 
-	private Vector3 destination;
 	private Transform arrowTransform;
+	private Vector3 destinationPosition;
+	private int destinationIndex;
+	private float originalAngle;
 
 	void Start () {
 		if (arrowTransform == null) {
 			arrowTransform = transform.GetChild (0);
 		}
+		destinationIndex = NULL;
 	}
 
 	void Update () {
 		if (arrowTransform.gameObject.activeInHierarchy) {
 			transform.localEulerAngles = Vector3.zero;
-			Vector3 realDirection = destination - transform.position;
+			Vector3 realDirection = destinationPosition - transform.position;
 			Vector3 pivotNormal = cameraTransform.position - transform.position;
 			Vector3 projectedDirection = Vector3.ProjectOnPlane (realDirection, pivotNormal);
-			if (projectedDirection.magnitude > MAGNITUDE_THRESHOLD) {
-				Vector3 arrowZeroPosition = arrowTransform.position - transform.position;
-				float angle = Vector3.SignedAngle (arrowZeroPosition, projectedDirection, pivotNormal);
-				transform.localEulerAngles = Vector3.back * angle;
-			} else {
-				arrowTransform.gameObject.SetActive (false);
-			}
+			Vector3 arrowZeroPosition = arrowTransform.position - transform.position;
+			float angle = Vector3.SignedAngle (arrowZeroPosition, projectedDirection, pivotNormal);
+			transform.localEulerAngles = Vector3.back * angle;
 		}
 	}
 
-	public void PointTo (Vector3 destination) {
-		this.destination = destination;
-		arrowTransform.gameObject.SetActive (true);
+	public void PointTo (Vector3 destinationPosition, int destinationIndex) {
+		this.destinationPosition = destinationPosition;
+		this.destinationIndex = destinationIndex;
+		if (destinationIndex != NULL) {
+			arrowTransform.gameObject.SetActive (true);
+		} else {
+			arrowTransform.gameObject.SetActive (false);
+		}
 	}
 
+	public void Enter (int objectIndex) {
+		if (objectIndex == destinationIndex) {
+			arrowTransform.gameObject.SetActive (false);
+		}
+	}
+
+	public void Exit () {
+		if (destinationIndex != NULL) {
+			arrowTransform.gameObject.SetActive (true);
+		}
+	}
 }
--- Assets/Scripts/VrUi.cs
+++ Assets/Scripts/VrUi.cs
@@ -8,10 +8,15 @@ public class VrUi : MonoBehaviour {
 	[SerializeField]
 	private Transform nextObject;
 	[SerializeField]
+	private int nextObjectIndex = -1;
+	[SerializeField]
 	private Transform prevObject;
+	[SerializeField]
+	private int prevObjectIndex = -1;
 
 	public void Open () {
 		gameObject.SetActive (true);
+		Pointer.Instance.PointTo (Vector3.zero, Pointer.NULL);
 	}
 
 	public void Close () {
@@ -19,11 +24,13 @@ public class VrUi : MonoBehaviour {
 	}
 
 	public void PointToNextObject () {
-		Pointer.Instance.PointTo (nextObject.position);
+		Pointer.Instance.PointTo (nextObject.position, nextObjectIndex);
+		Close ();
 	}
 
 	public void PointToPrevObject () {
-		Pointer.Instance.PointTo (prevObject.position);
+		Pointer.Instance.PointTo (prevObject.position, prevObjectIndex);
+		Close ();
 	}
-
+		
 }
