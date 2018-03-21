b93fb77b236c9d8892e2e7891d0bfd51f06017bb
Reinaldo Ignatius Wijaya <rei_iw@yahoo.com>
2018-03-21 19:56:25 +0700

    Added VR UI & sounds

---
+++ Assets/Scripts/VrUi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VrUi : MonoBehaviour {
+
+	[SerializeField]
+	private Transform nextObject;
+	[SerializeField]
+	private Transform prevObject;
+
+	public void Open () {
+		gameObject.SetActive (true);
+	}
+
+	public void Close () {
+		gameObject.SetActive (false);
+	}
+
+}
