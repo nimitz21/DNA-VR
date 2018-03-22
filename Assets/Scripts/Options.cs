31ef2168873b03eda5fdec5129ac054f4ef0b522
Reinaldo Ignatius Wijaya <rei_iw@yahoo.com>
2018-03-23 01:51:13 +0700

    Added options

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour {
+
+	[SerializeField]
+	private float ANGLE_THRESHOLD = 50f;
+	[SerializeField]
+	private float DISAPPEAR_TIME = 3f;
+	[SerializeField]
+	private Transform cameraTransform;
+
+	private bool willShow;
+	private GameObject canvas;
+	private bool isTimerRunning;
+	private float disappearTimer;
+
+	void Start () {
+		willShow = true;
+		canvas = transform.GetChild (0).gameObject;
+		isTimerRunning = false;
+	}
+
+	void Update () {
+		if (!canvas.activeInHierarchy) {
+			if (cameraTransform.eulerAngles.x >= ANGLE_THRESHOLD && willShow) {
+				canvas.SetActive (true);
+				willShow = false;
+			} else if (cameraTransform.eulerAngles.x < ANGLE_THRESHOLD) {
+				willShow = true;
+			}
+		} else {
+			if (cameraTransform.eulerAngles.x < ANGLE_THRESHOLD) {
+				canvas.SetActive (false);
+				willShow = true;
+			} else if (isTimerRunning) {
+				disappearTimer -= Time.deltaTime;
+				if (disappearTimer <= 0) {
+					canvas.SetActive (false);
+				}
+			}
+		}
+	}
+
+	public void ResetTimer () {
+		isTimerRunning = true;
+		disappearTimer = DISAPPEAR_TIME;
+	}
+
+	public void StopTimer () {
+		isTimerRunning = false;
+	}
+
+}
