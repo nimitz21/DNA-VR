3071fdd9b103e9838607beaa23034f322fa5d232
Reinaldo Ignatius Wijaya <rei_iw@yahoo.com>
2018-04-04 20:07:35 +0700

    Fixed prologue back button and added initial blocking panel

--- Assets/Scripts/Main Menu/Prologue.cs
+++ Assets/Scripts/Main Menu/Prologue.cs
@@ -12,11 +12,12 @@ public class Prologue : MonoBehaviour {
 	public class Transition {
 		public const int IDLE = 0;
 		public const int NEXT = 1;
-		public const int PREV = -1;
 	}
 
 	[SerializeField]
 	private float TRANSITION_SPEED = 5f;
+	[SerializeField]
+	private Image blockingPanelImage;
 	[SerializeField]
 	private List<Text> textList;
 
@@ -24,28 +25,31 @@ public class Prologue : MonoBehaviour {
 	private int transition = Transition.IDLE;
 
 	void Update () {
-		if (transition == Transition.IDLE) {
-			if (textList [currentPage].color.a < 1f) {
-				textList [currentPage].color = new Color (
-					textList [currentPage].color.r,
-					textList [currentPage].color.g,
-					textList [currentPage].color.b,
-					textList [currentPage].color.a + TRANSITION_SPEED * Time.deltaTime);
-			}
+		if (blockingPanelImage.color.a > 0) {
+			blockingPanelImage.color = new Color (0, 0, 0, blockingPanelImage.color.a - TRANSITION_SPEED * Time.deltaTime);
+			blockingPanelImage.gameObject.SetActive (false);
 		} else {
-			if (textList [currentPage].color.a > 0) {
-				textList [currentPage].color = new Color (
-					textList [currentPage].color.r,
-					textList [currentPage].color.g,
-					textList [currentPage].color.b,
-					textList [currentPage].color.a - TRANSITION_SPEED * Time.deltaTime);
+			if (transition == Transition.IDLE) {
+				if (textList [currentPage].color.a < 1f) {
+					textList [currentPage].color = new Color (
+						textList [currentPage].color.r,
+						textList [currentPage].color.g,
+						textList [currentPage].color.b,
+						textList [currentPage].color.a + TRANSITION_SPEED * Time.deltaTime);
+				}
 			} else {
-				currentPage += transition;
-				transition = 0;
-				if (currentPage == textList.Count) {
-					SceneController.Instance.LoadScene (FIRST_SCENE_NAME);
-				} else if (currentPage == NULL) {
-					GetComponent <ChildCanvas> ().Back ();
+				if (textList [currentPage].color.a > 0) {
+					textList [currentPage].color = new Color (
+						textList [currentPage].color.r,
+						textList [currentPage].color.g,
+						textList [currentPage].color.b,
+						textList [currentPage].color.a - TRANSITION_SPEED * Time.deltaTime);
+				} else {
+					currentPage += transition;
+					transition = 0;
+					if (currentPage == textList.Count) {
+						SceneController.Instance.LoadScene (FIRST_SCENE_NAME);
+					}
 				}
 			}
 		}
@@ -61,14 +65,12 @@ public class Prologue : MonoBehaviour {
 		}
 		currentPage = 0;
 		transition = Transition.IDLE;
+		blockingPanelImage.color = new Color (0, 0, 0, 1); 
+		blockingPanelImage.gameObject.SetActive (true);
 	}
 
 	public void NextPage () {
 		transition = Transition.NEXT;
-	}
-
-	public void PrevPage () {
-		transition = Transition.PREV;
 	}
 
 }
